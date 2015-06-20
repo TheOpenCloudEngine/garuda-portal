675bab8b5c4ef2251ff57319f7949b03af06d7b9
limhoo2015 <limhoo@uengine.org>
2015-06-19 18:37:50 -0700

    GarudaAllAppList.ejs를 추가

---
+++ garuda-portal-core/src/main/java/org/uengine/codi/mw3/model/GarudaAllAppList.java
@@ -0,0 +1,11 @@
+package org.uengine.codi.mw3.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by hoo.lim on 6/19/2015.
+ */
+@Component
+public class GarudaAllAppList extends AllAppList{
+
+}
