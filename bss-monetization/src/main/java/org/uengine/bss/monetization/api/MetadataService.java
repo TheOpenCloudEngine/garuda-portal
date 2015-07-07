291afe729031bfdb590697b8df9298e5170da918
limhoo2015 <limhoo@uengine.org>
2015-07-07 11:55:31 -0700

    이미지 메타데이타 가져오는 URL를 수정.

--- bss-monetization/src/main/java/org/uengine/bss/monetization/api/MetadataService.java
+++ bss-monetization/src/main/java/org/uengine/bss/monetization/api/MetadataService.java
@@ -8,17 +8,17 @@ import java.io.FileNotFoundException;
 /**
  * Created by hoo.lim on 6/29/2015.
  */
-@Path("/tenantImage")
+@Path("/")
 public interface MetadataService {
 
     @GET
-    @Path("/app/{appId}/text/{key}")
+    @Path("app/{appId}/text/{key}")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public Response getTextMetadata(@PathParam("appId") String appId, @PathParam("key") String key)
             throws FileNotFoundException;
 
     @GET
-    @Path("/{appId}/{key}")
+    @Path("{appId}/{key}")
     @Produces({"image/png", "image/jpg", "image/gif"})
     public Response getImageMetadata(@PathParam("appId") String appId,@PathParam("key") String key)
             throws FileNotFoundException;
